7b76e71c5a89c2a72c1def5c8bd95c0e448ab346
Oakware <i.o.kondratuk@gmail.com>
2018-10-31 20:55:30 +0200

    Updated router

--- src/app/router.jsx
+++ src/app/router.jsx
@@ -6,6 +6,7 @@ import Error404 from './homepage/Error404';
 import Register from './auth/Register';
 import Login from './auth/Login';
 import Profile from './profile/Profile';
+import ProfileSettings from './profile-settings/ProfileSettings';
 import ServiceCreator from './service-creator/ServiceCreator';
 import Service from './service/Service';
 
@@ -17,6 +18,7 @@ export default function router() {
                 <Route exact path="/auth/login" component={Login}/>
                 <Route exact path="/auth/register" component={Register}/>
                 <Route exact path="/profile/:userId" component={Profile}/>
+                <Route exact path="/profile/:userId/settings" component={ProfileSettings}/>
                 <Route exact path="/profile/:userId/create" component={ServiceCreator}/>
                 <Route exact path="/service/:serviceId" component={Service}/>
                 <Route component={Error404}/>
