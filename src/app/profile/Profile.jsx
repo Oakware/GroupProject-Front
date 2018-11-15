addbed00572437d23f5de48513c8894bb4470ec8
HGalitska <h.galitskaya@gmail.com>
2018-11-15 19:05:30 +0200

    changed Tabs to accept arrays

--- src/app/profile/Profile.jsx
+++ src/app/profile/Profile.jsx
@@ -10,11 +10,61 @@ export default class Profile extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = {userId: 0};
+        this.state = {
+            userId: 0,
+            services: {
+                my_services: [],
+                ordered_services: []
+            }
+        };
     }
 
     componentDidMount() {
-        this.setState({userId: this.props.match.params.userId})
+        this.setState({
+            userId: this.props.match.params.userId,
+            services: {
+                my_services: [
+                    {
+                        key: 1,
+                        id: 1,
+                        name: "Walk Your Dog",
+                        description: "Hamburger excepteur ex non. Picanha labore t-bone excepteur, shoulder jerky frankfurter jowl venison veniam andouille tail shank chicken prosciutto. Lorem et capicola pariatur frankfurter, fugiat turkey. Ex consequat dolore, eiusmod shank bacon tri-tip shoulder elit. Jowl rump tenderloin officia labore reprehenderit.",
+                        owner: "@iduchan0",
+                        mark: 3,
+                        price: 3
+                    },
+                    {
+                        key: 2,
+                        id: 2,
+                        name: "Feed Your Cat",
+                        description: "In t-bone salami occaecat tongue nostrud cupim dolore pancetta doner short ribs. Reprehenderit burgdoggen alcatra cupidatat non id laborum lorem andouille mollit. Chuck ham hock dolor ground round, esse porchetta kevin salami alcatra proident beef ribs incididunt anim nostrud ut. Pig cupim picanha frankfurter sint officia kielbasa qui.",
+                        owner: "@iduchan0",
+                        mark: 4,
+                        price: 3
+                    },
+                    {
+                        key: 3,
+                        id: 3,
+                        name: "Debug Your Code",
+                        description: "Bacon ipsum dolor amet deserunt officia in consectetur strip steak. Strip steak labore sint ham chuck buffalo, sunt velit reprehenderit andouille kevin. Pastrami velit jowl do voluptate turducken, landjaeger anim tongue dolor sirloin chicken et strip steak fatback. Frankfurter doner filet mignon minim, pancetta exercitation shank non chuck.",
+                        owner: "@iduchan0",
+                        mark: 3.5,
+                        price: 4
+                    },
+                    {
+                        key: 4,
+                        id: 4,
+                        name: "Merge Your Branches",
+                        description: "Ullamco dolor id laborum ham ham hock meatball consequat. In strip steak pork loin, nostrud short ribs aliquip nulla aliqua. Landjaeger biltong dolor ullamco. Nisi mollit pork chop in ut. Beef ribs frankfurter rump jowl voluptate drumstick.",
+                        owner: "@iduchan0",
+                        mark: 5,
+                        price: 5
+                    }
+                ],
+                ordered_services: [{key: 5, id: 5, name: "Title 5"}, {key: 7, id: 7, name: "Title 7"},
+                    {key: 8, id: 8, name: "Title 8"}, {key: 9, id: 9, name: "Title 9"}]
+            }
+        })
     }
 
     getUser(id) {
@@ -57,7 +107,7 @@ export default class Profile extends React.Component {
 
                         <ProfileTile profile={this.getUser(this.props.match.params.userId)}/>
 
-                        <Tabs userId={this.state.userId}/>
+                        <Tabs userId={this.state.userId} services={this.state.services}/>
                     </div>
                 </section>
             </main>
--- src/app/profile/Tabs.jsx
+++ src/app/profile/Tabs.jsx
@@ -28,56 +28,6 @@ export default class Tabs extends React.Component {
         );
     }
 
-    renderMyServices() {
-        //TODO: get data from back
-        let myServices = [
-            {
-                key: 1,
-                id: 1,
-                name: "Walk Your Dog",
-                description: "Hamburger excepteur ex non. Picanha labore t-bone excepteur, shoulder jerky frankfurter jowl venison veniam andouille tail shank chicken prosciutto. Lorem et capicola pariatur frankfurter, fugiat turkey. Ex consequat dolore, eiusmod shank bacon tri-tip shoulder elit. Jowl rump tenderloin officia labore reprehenderit.",
-                owner: "@iduchan0",
-                mark: 3,
-                price: 3
-            },
-            {
-                key: 2,
-                id: 2,
-                name: "Feed Your Cat",
-                description: "In t-bone salami occaecat tongue nostrud cupim dolore pancetta doner short ribs. Reprehenderit burgdoggen alcatra cupidatat non id laborum lorem andouille mollit. Chuck ham hock dolor ground round, esse porchetta kevin salami alcatra proident beef ribs incididunt anim nostrud ut. Pig cupim picanha frankfurter sint officia kielbasa qui.",
-                owner: "@iduchan0",
-                mark: 4,
-                price: 3
-            },
-            {
-                key: 3,
-                id: 3,
-                name: "Debug Your Code",
-                description: "Bacon ipsum dolor amet deserunt officia in consectetur strip steak. Strip steak labore sint ham chuck buffalo, sunt velit reprehenderit andouille kevin. Pastrami velit jowl do voluptate turducken, landjaeger anim tongue dolor sirloin chicken et strip steak fatback. Frankfurter doner filet mignon minim, pancetta exercitation shank non chuck.",
-                owner: "@iduchan0",
-                mark: 3.5,
-                price: 4
-            },
-            {
-                key: 4,
-                id: 4,
-                name: "Merge Your Branches",
-                description: "Ullamco dolor id laborum ham ham hock meatball consequat. In strip steak pork loin, nostrud short ribs aliquip nulla aliqua. Landjaeger biltong dolor ullamco. Nisi mollit pork chop in ut. Beef ribs frankfurter rump jowl voluptate drumstick.",
-                owner: "@iduchan0",
-                mark: 5,
-                price: 5
-            }
-        ]
-        return this.renderServiceTiles(myServices)
-    }
-
-    renderOrderedByMeServices() {
-        //TODO: get data from back
-        let orderedByMeServices = [{key: 5, id: 5, name: "Title 5"}, {key: 7, id: 7, name: "Title 7"},
-            {key: 8, id: 8, name: "Title 8"}, {key: 9, id: 9, name: "Title 9"}]
-        return this.renderServiceTiles(orderedByMeServices)
-    }
-
     renderTabsNames(){
         //TODO: ID of actually logged in user
         var currentUserId = 1
@@ -109,12 +59,12 @@ export default class Tabs extends React.Component {
                 <div>
                     <div id="MyServContent" className="content-tab">
                         <div className="columns is-multiline is-centered">
-                            {this.renderMyServices()}
+                            {this.renderServiceTiles(this.props.services.my_services)}
                         </div>
                     </div>
                     <div id="MyOrdContent" className="content-tab" style={{display: "none"}}>
                         <div className="columns is-multiline is-centered">
-                            {this.renderOrderedByMeServices()}
+                            {this.renderServiceTiles(this.props.services.ordered_services)}
                         </div>
                     </div>
                 </div>
