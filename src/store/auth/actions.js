aee54f72c7afa62ca5ba04379f501a45bcb9e03d
Oakware <i.o.kondratuk@gmail.com>
2018-11-24 22:00:54 +0200

    Updated auth

--- src/store/auth/actions.js
+++ src/store/auth/actions.js
@@ -4,25 +4,22 @@ import * as Auth from '../../microservices/auth';
 
 export function initAuth() {
     return async (dispatch, getState) => {
-        let keycloak = Auth.configureKeycloak();
-        keycloak.init({ onLoad: 'check-sso', flow: 'implicit' }).success(authenticated => {
-            dispatch({
-                type: types.KEYCLOAK_INITIALIZED,
-                keycloak,
-                authenticated
-            });
+        let res = await Auth.configureKeycloak();
+        let { keycloak, authenticated } = res;
 
-            if (authenticated) {
-                keycloak.loadUserProfile().success(profile => {
-                    dispatch({
-                        type: types.USER_PROFILE_FETCHED,
-                        profile
-                    });
-                }).error(console.error);
-            }
-        }).error(e => {
-            console.error('Keycloak: failed to initialize', e);
+        dispatch({
+            type: types.KEYCLOAK_INITIALIZED,
+            keycloak,
+            authenticated
         });
+
+        if (authenticated) {
+            let profile = await Auth.loadUserProfile();
+            dispatch({
+                type: types.USER_PROFILE_FETCHED,
+                profile
+            });
+        }
     };
 }
 
